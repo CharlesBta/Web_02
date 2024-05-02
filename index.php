7cda7fe2b4a89f6532b5128b817915a6170e748c
Charles-Antoine <charto.bta@gmail.com>
2024-05-02 13:45:26 +0200

    partie3

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
 <?php
-    echo "Hello World!";
+global $dbPDO;
+require_once('pdo.php');
 ?>
+
+<!<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Jersey+15&display=swap');
+        * {
+            font-family: "Jersey 15", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+        }
+    </style>
+    <title>Web_02</title>
+</head>
+<body>
+<?php
+    $res = $dbPDO->prepare("
+    SELECT titre, Genre.libelle as genre, Realisateur.prenom as prenom, Realisateur.nom as nom, YEAR(dateDeSortie) as dateDeSortie FROM Film
+    INNER JOIN Genre ON Film.id_genre = Genre.id
+    INNER JOIN Realisateur ON Film.id_realisateur = Realisateur.id
+    WHERE dateDeSortie BETWEEN '2010-01-01' AND '2019-12-31'
+    ");
+    $req = $res->execute();
+?>
+<h1>Liste des meilleurs films des années 2010 :</h1>
+<ul>
+    <?php
+        foreach ($res->fetchAll(PDO::FETCH_CLASS) as $film) {
+            echo "
+            <li>
+            <a href='*'>$film->titre</a> ($film->genre de $film->prenom $film->nom, $film->dateDeSortie)
+            </li>
+            ";
+        }
+    ?>
+</ul>
+</body>
+</html>
